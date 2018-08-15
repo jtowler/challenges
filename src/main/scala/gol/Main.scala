32ea1631c67fbd873724556ed1a9e1543cf6ce4e
themother <jacktowleretc@hotmail.com>
2018-08-15 21:44:30 +0100

    basic working board

--- src/main/scala/gol/Main.scala
+++ src/main/scala/gol/Main.scala
@@ -2,7 +2,8 @@ package gol
 
 object Main extends App{
 
-  val board = Board(10, 10, List((1,1)))
+  val board = Board(10, 10, List((1,1), (2,1), (3,1)))
   board.display()
-
+  val board2 = board.getNextIteration
+  board2.display()
 }
