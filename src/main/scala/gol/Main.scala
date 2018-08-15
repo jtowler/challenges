4949a8c977554245822fcdcaa3b2d78f3d8984d8
themother <jacktowleretc@hotmail.com>
2018-08-15 21:54:50 +0100

    added mainloop

--- src/main/scala/gol/Main.scala
+++ src/main/scala/gol/Main.scala
@@ -3,7 +3,15 @@ package gol
 object Main extends App{
 
   val board = Board(10, 10, List((1,1), (2,1), (3,1)))
-  board.display()
-  val board2 = board.getNextIteration
-  board2.display()
+
+  def loop(board: Board): Unit = {
+    if (board.deadBoard) ()
+    else {
+      board.display()
+      loop(board.getNextIteration)
+    }
+  }
+
+  loop(board)
+
 }
