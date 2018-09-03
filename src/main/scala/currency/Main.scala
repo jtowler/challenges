2892bb6596fab6bde6383500e8c912a35beae93e
themother <jacktowleretc@hotmail.com>
2018-09-03 07:43:14 +0100

    completed with unremoved warts

--- src/main/scala/currency/Main.scala
+++ src/main/scala/currency/Main.scala
@@ -8,22 +8,34 @@ import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
 
 object Main extends App {
 
-  def convert(from: String, to: String): Unit = {
+  @specialized def registerModule[A](evaluateForSideEffectOnly: A): Unit = {
+    val _: A = evaluateForSideEffectOnly
+    () //Return unit to prevent warning due to discarding value
+  }
 
-    val url = f"http://free.currencyconverterapi.com/api/v5/convert?q=${from}_$to&compact=y"
+  def convert(currStr: String): Double = {
+
+    val url: String = f"http://free.currencyconverterapi.com/api/v5/convert?q=" + currStr + "&compact=y"
     val result: BufferedSource = scala.io.Source.fromURL(url)
     val mapper = new ObjectMapper() with ScalaObjectMapper
-    mapper.registerModule(DefaultScalaModule)
-    val parsedJson = mapper.readValue[Map[String, Object]](result.reader())
-    println(parsedJson)
+    registerModule(mapper.registerModule(DefaultScalaModule))
+    val parsedJson = mapper.readValue[Map[String, Map[String, Double]]](result.reader())
+    val level1: Map[String, Double] = parsedJson.getOrElse(currStr, Map("val" -> -1.0))
+    level1.getOrElse("val", -1.0)
+  }
+
+  def displayConversion(from: String, to: String, amount: Double): Unit = {
+    val currStr = from + "_" + to
+    val rate = convert(currStr)
+    val convertedAmount = amount * rate
+    val convertedString = "%.2f".format(convertedAmount).toDouble
+   registerModule( println(f"$amount $from is equivalent to $convertedString $to"))
   }
 
   val from = "USD"
   val to = "GBP"
   val amount = 100
 
-//  val rate = convert(from, to)
-//
-//  print(amount * rate)
+  displayConversion(from, to, amount)
 
 }
