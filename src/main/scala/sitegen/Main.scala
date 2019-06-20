b241b6073f45d53b0da34d1154a7f1f4a81bdacd
themother <jacktowleretc@hotmail.com>
2019-06-20 17:56:04 +0100

    added a few more objects

--- src/main/scala/sitegen/Main.scala
+++ src/main/scala/sitegen/Main.scala
@@ -2,9 +2,21 @@ package sitegen
 
 object Main extends App {
 
-  val head = Head("The simplest HTML example")
-  val heading = Heading("This is an HTML Page", 1)
-  val body = Body(heading)
-  val page = new Page(head, body)
+
+  val page = Page(
+      Head("Your Title Here"),
+      Body(
+          List[HTML](
+              Heading("This is a heading", 1),
+              HR(),
+              Heading("This is another heading", 2),
+              Paragraph(
+                "An image mixed in with text",
+                List(Image("/Users/jack/Documents/1473136640578.png"))
+              )
+          )
+      )
+  )
+
   println(page.getContents)
 }
